89a3eb74fc5b58853bcded6b7b323bd3862dad54
Lasse Sjørup <lasse.sjorup@danfoss.com>
2019-02-03 00:56:06 +0100

    Updating version number

--- UrlReplace.Fiddler2/Properties/AssemblyInfo.cs
+++ UrlReplace.Fiddler2/Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Fiddler2 bulk UrlReplace")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyProduct("Fiddler bulk UrlReplace")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.*")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
